de4722c1b511c5abc0aa0135f4eaffc1b4350467
MIHIRSHARMA2000 <mihirsharma@deloitte.com>
2022-04-06 12:35:51 +0530

    Solution2 File Created

---
+++ FilterAssignment/Solution2.py
@@ -0,0 +1,9 @@
+import functools
+list  = []
+n = int(input("Enter number of elements : "))
+for i in range(0, n):
+    ele = int(input())
+    list.append(ele)
+output_reduce = functools.reduce(lambda x, y: x * y, list)
+print(output_reduce)
+print()
